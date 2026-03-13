f7bb2e7c4dacd6b0edacd5fdba4aa688b1356f08
Jedidja Mansvelder <jedidja@MacBookPro.home>
2026-03-13 10:55:51 +0100

    changed global style to all instead of just body. fiexed a typo in app.ts changed section into just the footer tag

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('matt-fdffarley');
+  protected readonly title = signal('matt-farley');
 }
